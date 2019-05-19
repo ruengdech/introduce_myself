f6deaae4bb4c1dc81bdd3876c7add50c2fdf8724
ruengdech <ruengdech@gmail.com>
2019-05-19 14:07:25 +0700

    First Upload for Test purpose
    this is optional first Git-hub

---
+++ __init__.py
@@ -0,0 +1,2 @@
+#__init_.py # this is require
+from ruengdech.myname import my_info #this is
---
+++ myname.py
@@ -0,0 +1,6 @@
+#myname.py
+#this is my Test first Pypi
+def my_info():
+	print('Name: Ruengdech La-orpongpaisan')
+	print('Tel: 6681443772')
+	print('Email: ruegndech@gmail.com')
